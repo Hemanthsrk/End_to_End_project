198b554666f0525803a6bd1e117442963c1db707
Hemanth katikareddy <144310110+Hemanthsrk@users.noreply.github.com>
2026-08-31 15:23:44 +0530

    Update inputs.tf

--- Terraform/inputs.tf
+++ Terraform/inputs.tf
@@ -1,1 +1,88 @@
 
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "front-backend"
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+
+  default = [
+    "ap-south-1a",
+    "ap-south-1b"
+  ]
+}
+
+variable "eks_cluster_version" {
+  description = "EKS Kubernetes version"
+  type        = string
+  default     = "1.33"
+}
+
+variable "node_instance_types" {
+  description = "EKS worker node instance types"
+  type        = list(string)
+
+  default = [
+    "t3.medium"
+  ]
+}
+
+variable "node_min_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_max_size" {
+  type    = number
+  default = 4
+}
+
+variable "node_desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "db_name" {
+  description = "RDS database name"
+  type        = string
+  default     = "employees"
+}
+
+variable "db_username" {
+  description = "RDS database username"
+  type        = string
+  default     = "postgres"
+}
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_allocated_storage" {
+  description = "RDS allocated storage"
+  type        = number
+  default     = 20
+}
